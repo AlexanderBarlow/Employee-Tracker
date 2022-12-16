abd9336028158dbfc5b754c80e38941249462d1c
alex barlow <alex.barlow@rocketmail.com>
2022-12-16 14:30:07 -0500

    refactoring schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE employee_db;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL  PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL  PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
@@ -25,9 +25,8 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
-    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,21 @@
-INSERT INTO department (name)
-VALUES ('Management');
-INSERT INTO department (name)
-VALUES ('Sales');
-INSERT INTO department (name)
-VALUES ('Sales');
+INSERT INTO department (id, name)
+VALUES ('1', 'Management');
+INSERT INTO department (id, name)
+VALUES ('2', 'Sales');
+INSERT INTO department (id, name)
+VALUES ('3', 'Accounting');
 
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ('Manager', '85.000', 1);
-INSERT INTO roles (title, salary, department_id)
-VALUES ('Sales', '65.000', 2);
-INSERT INTO roles (title, salary, department_id)
-VALUES ('Sales', '65.000', 3);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ('1', 'Manager', '85.000', 1);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ('2', 'Sales-Associate', '65.000', 2);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ('3', 'Accountant', '65.000', 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Michael', 'Scott', 1,  null);
-INSERT INTO employee (first_name, last_name, role_id,  manager_id)
-VALUES ('Jim', 'Halpert', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id,  manager_id)
-VALUES ('Dwight', 'Schrute', 3,  1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jim', 'Halpert',  2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dwight', 'Schrute', 2, 1);
